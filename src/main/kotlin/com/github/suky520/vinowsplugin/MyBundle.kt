db413f0aabba10d6b3af38cd8ed81f646d962a48
GitHub Action <action@github.com>
2024-08-15 10:23:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/suky520/vinowsplugin/MyBundle.kt
+++ src/main/kotlin/com/github/suky520/vinowsplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.suky520.vinowsplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/suky520/vinowsplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/suky520/vinowsplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.suky520.vinowsplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
